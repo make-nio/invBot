e55e6bdc6fabee6f745e048a72c0e2822a69e98a
Mariano Sanchez <marianorsanchez@gmail.com>
2023-09-15 04:07:05 -0300

    otras correcciones

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    roots: ['<rootDir>/backend/src/'],
-    transform: {
-      '^.+\\.ts?$': 'ts-jest'
-    },
-    testRegex: '/__tests__/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/backend/src/'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest'
+  },
+  testRegex: '/__tests__/.*\\.(test|spec)?\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+};
